e5df221c7a10fec7e03dd03d5f9b95e60269fe3a
gaunhaha <gaunhaha@gmail.com>
2024-11-06 23:59:46 +0800

    up

--- src/layout/footer/Footer.vue
+++ src/layout/footer/Footer.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="h-16 flex justify-end items-center p-3 bg-cyan-600">
         <span>
+            <i class="fa fa-envelope me-1"></i>
             <span class="me-2">
                 contact me
             </span>
