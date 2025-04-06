7556cb9191490f4b1ca8ef527734c181976cf0f5
kanta <gaunhaha@gmail.com>
2025-04-06 14:19:16 +0800

    修正專案元件的檔案名稱，並新增專案展示頁面，包含多個網站開發項目的詳細資訊與連結，提升使用者對專案的了解與互動性。

--- src/views/skill/Skill.vue
+++ src/views/skill/Skill.vue
@@ -38,7 +38,7 @@
 import i18n from "@/core/plugins/i18n/i18n.ts";
 import WorkExperience from "./workExperience/WorkExperience.vue";
 // import SideProject from "./sideProject/SideProject.vue";
-import Project from "./project/Pproject.vue";
+import Project from "./project/Project.vue";
 import SkillList from "./skillList/skillList.vue";
 import OtherExperience from "./otherExperience/OtherExperience.vue";
 import University from "./university/University.vue";
